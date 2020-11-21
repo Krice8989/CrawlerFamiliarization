cdb8795a9e6094028eabf222a2a7300ac83b2083
Kam <Krice8989@gmail.com>
2020-11-21 18:22:00 -0500

    Add project files.

---
+++ CrawlerFamiliarization/HtmlAgilityDemo.cs
@@ -0,0 +1,65 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+
+namespace CrawlerFamiliarization
+{
+    public class HtmlAgility
+    {
+        private List<Uri> _frontier = new List<Uri>();
+        private HtmlWeb _web = new HtmlWeb();
+        private HtmlDocument _doc;
+
+        public void Crawl()
+        {
+            BuildInitialFrontier();
+            ExpandFront();
+        }
+
+        private void BuildInitialFrontier()
+        {
+            Console.Write("Input Initial Seed: ");
+            var input = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(input))
+            {
+                Console.Write("Invalid Input...\n\nInput Initial Seed:");
+                input = Console.ReadLine();
+            }
+
+            Uri uri = new Uri(input);
+
+            _doc = _web.Load(uri);
+
+            foreach (HtmlNode node in _doc.DocumentNode.SelectNodes("//a"))
+            {
+                if (string.IsNullOrEmpty(node.Attributes["href"].Value.ToString()) || !node.Attributes["href"].Value.ToString().Contains("http"))
+                {
+                    continue;
+                }
+                _frontier.Add(new Uri(node.Attributes["href"].Value.ToString()));
+
+            }
+        }
+
+        private void ExpandFront()
+        {
+            for (int i = 0; i < _frontier.Count; i ++)
+            {
+                _doc = _web.Load(_frontier[i]);
+                foreach (HtmlNode node in _doc.DocumentNode.SelectNodes("//a"))
+                {
+                    if (node?.Attributes?["href"].Value == null || string.IsNullOrEmpty(node.Attributes["href"].Value.ToString()) || !node.Attributes["href"].Value.ToString().Contains("http"))
+                    {
+                        continue;
+                    }
+                    _frontier.Add(new Uri(node.Attributes["href"].Value.ToString()));
+                    Console.WriteLine(_frontier.Count);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\n\n {_frontier.Count} Links found.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ CrawlerFamiliarization/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CrawlerFamiliarization
+{
+    partial class Program
+    {
+        static void Main()
+        {
+            HtmlAgility htmlAgility = new HtmlAgility();
+            htmlAgility.Crawl();
+        }
+    }
+}
