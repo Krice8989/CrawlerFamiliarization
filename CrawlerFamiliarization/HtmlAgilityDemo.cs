8d3183c4fc016cdacee6ec2f0e420b24d0c1e280
Kam <Krice8989@gmail.com>
2020-11-21 20:17:53 -0500

    Added.

--- CrawlerFamiliarization/HtmlAgilityDemo.cs
+++ CrawlerFamiliarization/HtmlAgilityDemo.cs
@@ -44,21 +44,45 @@ namespace CrawlerFamiliarization
 
         private void ExpandFront()
         {
-            for (int i = 0; i < _frontier.Count; i ++)
+            for (int i = 0; i < _frontier.Count; i++)
             {
                 _doc = _web.Load(_frontier[i]);
+
+                if (_doc.DocumentNode.SelectNodes("//a") == null)
+                {
+                    continue;
+                }
+
+                string newlink = _doc.DocumentNode.SelectNodes("//a").ToString();
+
                 foreach (HtmlNode node in _doc.DocumentNode.SelectNodes("//a"))
                 {
-                    if (node?.Attributes?["href"].Value == null || string.IsNullOrEmpty(node.Attributes["href"].Value.ToString()) || !node.Attributes["href"].Value.ToString().Contains("http"))
+                    if (node.Attributes["href"]?.Value == null)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("LINK IGNORED --- CASE: INVALID VALUE. --- LINK: Null");
                         continue;
                     }
+                    if (!node.Attributes["href"].Value.ToString().StartsWith("http"))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"LINK IGNORED --- CASE: NO HTTP. --- LINK: {node.Attributes["href"].Value.ToString()}");
+                        continue;
+                    }
+                    if (_frontier.Contains(new Uri(node.Attributes["href"].Value.ToString())))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"LINK IGNORED --- CASE: DUPLICATE --- LINK: {node.Attributes["href"].Value.ToString()}");
+                        continue;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
                     _frontier.Add(new Uri(node.Attributes["href"].Value.ToString()));
-                    Console.WriteLine(_frontier.Count);
+                    Console.WriteLine($"New Link Found: {node.Attributes["href"].Value.ToString()}---Total Links: {_frontier.Count}");
                 }
             }
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"\n\n {_frontier.Count} Links found.");
+            Console.WriteLine($"\n/n{_frontier.Count} Links found.");
             Console.ForegroundColor = ConsoleColor.White;
         }
     }
